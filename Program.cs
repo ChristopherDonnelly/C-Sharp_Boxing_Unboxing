d1d85f3943b4691e1275672c15e1a0cafddcdefb
Christopher Donnelly <christopher.d.donnelly@gmail.com>
2018-04-02 09:47:27 -0700

    Commit C# Boxing/Unboxing assignment

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> box = new List<object>();
+            box.Add(7);
+            box.Add(28);
+            box.Add(-1);
+            box.Add(true);
+            box.Add("chair");
+
+            int sum = 0;
+
+            for(int i = 0; i < box.Count; i++){
+                if(box[i] is int){
+                    sum += (int)box[i];
+                }
+            }
+
+            Console.WriteLine("The Sum is: " + sum);
+
+            foreach (object vals in box){
+                Console.WriteLine(" - " + vals);
+            }
+        }
+    }
+}
